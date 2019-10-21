b9cfa916ec8fb3a1613ff8d988deae80258b0136
wellingtonsilva04 <wel.dss@gmail.com>
2019-10-21 03:02:52 -0300

    upgrade react-native@0.61.2. bug #1 coorigido com upgrade

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,12 @@
 import { createStore, compose, applyMiddleware} from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import rootReducer from './reducers';
 
 const persistConfig = {
-  key: 'root',
-  storage: storage,
-  stateReconciler: autoMergeLevel2 // see "Merge Process" section for details.
+  key: '@root',
+  storage: AsyncStorage,
  };
  const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
  const middleWare = [];
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'; 
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import {  createAppContainer } from "react-navigation";
+import {createStackNavigator} from "react-navigation-stack";
 import HomeScreen from "./screens/home";
 import AddDisciplina from "./screens/inserirDisciplina"
 import Disciplina from './screens/Disciplina';
